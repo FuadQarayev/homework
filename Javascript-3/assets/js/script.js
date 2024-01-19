50b9338374ef7e034b4fe343bd3364761a2f8dfc
FuadQarayev <fuadelisli6@gmail.com>
2024-01-20 00:43:31 +0400

    task add

---
+++ Javascript-3/assets/js/script.js
@@ -0,0 +1,58 @@
+// ? Task1
+// let numbers=[3 , 4, 5, 6]
+// console.log(numbers.join("  "))
+// ? Task2
+// let metn="Mastercode training academy"
+// console.log(metn)
+// console.log(metn.split(""))
+// ? Task3
+// let arr=[3,4,5,6]
+// cem=0
+// hasil=1
+// for(i=0;i<arr.length; i++){
+//     cem+=arr[i]
+//     hasil*=arr[i]
+// }
+// console.log(cem)
+// console.log(hasil)
+// ? Task4
+// const array = [3, 4, 7, 9, 11, 33, 21, 11, 3, 7, 27, 45, 76, 9];
+// const uniqueArray = array.filter((value, index, self) => {
+//   return self.indexOf(value) === index;
+// });
+// console.log(uniqueArray);
+// ? Task6
+// let word=['Mastercode'];
+// console.log(word);
+// console.log(word.reverse());
+// ? Task7
+// let metn=['Mastercode', 'training', 'academy'];
+// console.log(metn);
+// console.log(metn.reverse());
+// ? Task9
+// let metn="Mastercode training academy"
+// console.log(metn)
+// console.log(metn.split(" "))
+// ? Task10
+// let metn="     Mastercode training academy      "
+// console.log(metn)
+// console.log(metn.trim())
+// ? Task11
+// let metn="Mastercode Training Academy"
+// console.log(metn.toUpperCase("Mastercode"))
+// ? Task13
+// let arr =  [3, 4, 7, 9, 11, 33, 21, 11, 3, 7, 27, 45, 76, 9]
+// console.log(arr.sort(
+//     function(a,b){
+//         return a-b
+//     }
+// ))
+// console.log(arr.sort(
+//     function(a,b){
+//         return b-a
+//     }
+// ))
+// ? Task15
+// let metn="Dunya 71 % sudan, 29 % ise qurudan ibaretdir"
+// console.log(metn)
+// console.log(metn.replace("%", "faiz"))
